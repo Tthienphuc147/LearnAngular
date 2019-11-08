c20d083f4b4b843967162fef2479c01a3bd652bf
Tthienphuc147 <43287192+Tthienphuc147@users.noreply.github.com>
2019-11-08 22:58:59 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
 
-  { path: '', loadChildren:()=> import('./PackagePermission/PackagePermission.module').then(m=>m.PackagePermisionModule) },
+  { path: 'package', loadChildren:()=> import('./PackagePermission/PackagePermission.module').then(m=>m.PackagePermisionModule) },
   { path: 'create-form', loadChildren: () => import('./create-form/create-form.module').then(m => m.CreateFormModule) },
-  { path: 'login',  loadChildren: () => import('./formlogin/formlogin.module').then(m => m.FormLoginModule)  }
+  { path: '',  loadChildren: () => import('./formlogin/formlogin.module').then(m => m.FormLoginModule)  }
 
 
 ];
